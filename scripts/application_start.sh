e85d7c47fd13002b0b72a6cd699585a31086e3b6
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-04 12:47:04 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart app.js' >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart express-app >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 restart app >> /home/ubuntu/nodejs-aws-codedeploy-pipeline/deploy.log
